49c7555158433a6957a4020db62b15c75ebba890
BartlomiejRasztabiga <contact@rasztabiga.me>
2021-05-22 12:33:43 +0200

    Add message model

--- app/models/message.py
+++ app/models/message.py
@@ -1,7 +1,9 @@
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, String
 
 from app.db.base_class import Base
 
 
 class Message(Base):
     id = Column(Integer, primary_key=True, index=True)
+    content = Column(String(length=160), nullable=False)
+    views_count = Column(Integer, nullable=False, default=0)
